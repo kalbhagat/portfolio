10e57ce47483d0dbab016da3a4a59ee4b662cae1
[Priyanka Bhagat] <[p.bhagat0312@gmail.com]>
2022-09-11 20:20:16 +0100

    first commit

---
+++ src/app/core/providers/skills-mock.ts
@@ -0,0 +1,12 @@
+import { Skill } from './skill';
+
+export const SKILLS: Skill[] = [
+  { id: 3, name: 'assets/images/PSM.png', altText: 'PSM' },
+  { id: 5, name: 'assets/images/java.png', altText: 'JAVA' },
+  { id: 6, name: 'assets/images/flutter.png', altText: 'FLUTTER' },
+  { id: 8, name: 'assets/images/sql.png', altText: 'SQL' },
+  { id: 4, name: 'assets/images/javascript.png', altText: 'JAVASCRIPT' },
+  { id: 1, name: 'assets/images/html5.png', altText: 'HTML' },
+  { id: 2, name: 'assets/images/css3.png', altText: 'CSS' },
+  { id: 7, name: 'assets/images/react.png', altText: 'REACT' },
+];
